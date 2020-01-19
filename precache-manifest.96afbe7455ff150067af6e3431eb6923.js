11d1bbe47821e85ef980a602d7dc11470bace1ea
Jhon Raymond <horse2019@protonmail.com>
2020-01-19 11:14:23 +0800

    Updates

---
+++ precache-manifest.96afbe7455ff150067af6e3431eb6923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee141a8f13756a050fea860646a236a4",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "55b6155d97262431e1d3",
+    "url": "/first-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c02e8d815ed9674347bc",
+    "url": "/first-react/static/js/2.5a540b83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-react/static/js/2.5a540b83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55b6155d97262431e1d3",
+    "url": "/first-react/static/js/main.5d3f355b.chunk.js"
+  },
+  {
+    "revision": "0a90e14be00101edfc47",
+    "url": "/first-react/static/js/runtime-main.1b73f01b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
